374a4162b5d55f6227febda928c78aea925f8261
Nicolin Danut <nicolindanut@gmail.com>
2023-02-28 15:21:43 +0200

    added print from main

--- a..py
+++ a..py
@@ -1,13 +1,1 @@
-print("sadas")
-print("some change made in the test-branch")
-<<<<<<< HEAD
-print("asdasd")
-=======
-print("asdasd")
-
-print("ASdasd")
-<<<<<<< HEAD
->>>>>>> test-branch
-=======
-print('aaaaaaaaaaa')
->>>>>>> test-branch
+print("print from main")
